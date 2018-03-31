45afa6a2b2097a3d69024e20624805fdd3c338a7
salaros <salaros@yandex.ru>
2018-04-01 00:56:48 +0300

    remove the loading spinner only when the last CSS file has been loaded

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 function loadDeferredStyles() {
     var head = document.head || document.getElementsByTagName('head')[0],
         stylesDeferred = document.querySelector('.deferred.styles');
+
     head.innerHTML += stylesDeferred.textContent;
     stylesDeferred.parentElement.removeChild(stylesDeferred);
+
+    var lastStyleSentinel = document.querySelector('link:last-child');
+    lastStyleSentinel.onload = revealRealContent;
+    lastStyleSentinel.onerror = revealRealContent;
 };
 
 function revealRealContent() {
-    var loadingProgress = document.querySelector('.loading.progress');
-    loadingProgress.style.display = 'none';
+    var visibleDelay = 250,
+        loadingProgress = document.querySelector('.loading.progress');
+    window.setTimeout(function() {
+        loadingProgress.style.display = 'none';
+    }, visibleDelay);
 }
 
 var reqAniFrame =
@@ -17,5 +25,3 @@ if (reqAniFrame)
     reqAniFrame(loadDeferredStyles);
 else
     window.addEventListener('load', loadDeferredStyles);
-
-window.setTimeout(revealRealContent, 1000);
